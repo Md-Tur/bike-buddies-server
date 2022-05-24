caa72aedd418180915c524f33d5222034630b717
Md-Tur <turibnmostafiz@gmail.com>
2022-05-24 23:34:06 +0600

    data loaded from mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,6 +16,15 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('bike_buddies').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
     }
     finally {
 
