7d2ef256ae94dd451514bf296b5ccb0ff2c846b5
Md-Tur <turibnmostafiz@gmail.com>
2022-05-25 17:06:08 +0600

    make admin API implemented

--- index.js
+++ index.js
@@ -87,6 +87,17 @@ async function run() {
             res.send(users);
         });
 
+        // making admin
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
     }
     finally {
 
