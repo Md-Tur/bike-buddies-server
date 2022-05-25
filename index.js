1bfa767b7cf04cf9153ced480d0d110fb62bdad4
Md-Tur <turibnmostafiz@gmail.com>
2022-05-25 13:18:33 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,15 @@ async function run() {
         await client.connect();
         const productsCollection = client.db('bike_buddies').collection('products');
         const orderCollection = client.db('bike_buddies').collection('orders');
+
+        //JWT token
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
 
         app.get('/product', async (req, res) => {
             const query = {};
