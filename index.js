eab3564b45a737102ef1c71c3eef9a802569933e
Md-Tur <turibnmostafiz@gmail.com>
2022-05-26 15:23:10 +0600

    route specification based on admin or user

--- index.js
+++ index.js
@@ -123,15 +123,30 @@ async function run() {
             res.send(users);
         });
 
-        // making admin
-        app.put('/user/admin/:email', async (req, res) => {
+        // checking if admin or normal user & showing route
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
+
+        // making admin
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' });
+            }
         });
 
     }
