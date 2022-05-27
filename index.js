ae2a43074f9fa41f42ebef468dd2450d49540dde
Md-Tur <turibnmostafiz@gmail.com>
2022-05-27 17:09:56 +0600

    cors issues

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
-
 const corsConfig = {
-    origin: '*',
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
@@ -21,7 +19,6 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
     next()
 })
-app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yvqsm.mongodb.net/?retryWrites=true&w=majority`;
 
