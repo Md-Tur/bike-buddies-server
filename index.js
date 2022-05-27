429fd9912d0559d42bddee43fa15c1c6ea21f683
Md-Tur <turibnmostafiz@gmail.com>
2022-05-27 12:41:04 +0600

    origin fixed

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yvqsm.mongodb.net/?retryWrites=true&w=majority`;
