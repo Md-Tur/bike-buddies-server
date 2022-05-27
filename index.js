32ebf1900b610f2f446a655faf826ed313201eff
Md-Tur <turibnmostafiz@gmail.com>
2022-05-27 16:31:07 +0600

    cors

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 
 const corsConfig = {
-    origin: '*',
+    origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsConfig))
-app.options("*", cors(corsConfig))
-app.use(express.json())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
-    next()
-})
+app.options('*', cors(corsConfig))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yvqsm.mongodb.net/?retryWrites=true&w=majority`;
